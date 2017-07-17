07101d33c36aca3a2c221965ba2e56ff28029e4f
yanze417 <417447576@qq.com>
2017-07-17 23:58:02 +0800

    新增findUserByUserName()\updateLoginInfo()方法

--- src/main/java/com/smart/dao/UserDao.java
+++ src/main/java/com/smart/dao/UserDao.java
@@ -1,9 +1,13 @@
 package com.smart.dao;
 
+import com.smart.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowCallbackHandler;
 import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.prefs.Preferences;
 
 /**
@@ -13,8 +17,11 @@ import java.util.prefs.Preferences;
 public class UserDao {
 
     private JdbcTemplate jdbcTemplate;
+    //根据用户名查询用户的sql
+    private final static String MATCH_COUNT_SQL = "";
+    private final  static String UPDATE_LOGIN_INFO_SQL ="";
 
-        @Autowired
+    @Autowired
     public void setJdbcTemplate (JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
 
@@ -24,5 +31,23 @@ public class UserDao {
         String sqlStr = "SELECT COUNT(*) FROM t_user WHERE user_name = ? and password = ?";
         Object[] a = new Object[] {userName, password};
         return jdbcTemplate.queryForObject(sqlStr, a, Integer.class);
+    }
+
+    public User findUserByUserName (final String userName) {
+        final User user = new User();
+        jdbcTemplate .query(MATCH_COUNT_SQL, new Object[]{userName},
+                new RowCallbackHandler() {
+                    public void processRow(ResultSet resultSet) throws SQLException {
+                        user.setUserId(resultSet.getInt("user_id"));
+                        user.setUserName(userName);
+                        user.setCredits(resultSet.getInt("credits"));
+                    }
+                });
+        return user;
+    }
+
+    public void updateLoginInfo (User user) {
+        jdbcTemplate.update(UPDATE_LOGIN_INFO_SQL, new Object[] {user.getLastVisit(),
+        user.getLastIp(), user.getCredits(), user.getUserId()});
     }
 }
