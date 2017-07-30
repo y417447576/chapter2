fe299b6646b5bf6fb5ccf6b2cb8c18347960cd43
yanze417 <417447576@qq.com>
2017-07-30 22:15:54 +0800

    测试用例完成，web.xml配置完成，增加controller包

---
+++ src/main/java/com/smart/web/LoginController.java
@@ -0,0 +1,7 @@
+package com.smart.web;
+
+/**
+ * Created by Administrator on 2017/7/30/030.
+ */
+public class LoginController {
+}
--- src/test/java/com/smart/service/UserServiceTest.java
+++ src/test/java/com/smart/service/UserServiceTest.java
@@ -1,35 +1,37 @@
 package com.smart.service;
 
-import org.junit.Assert;
-import org.junit.Test;
+import java.util.Date;
+import org.springframework.test.context.testng.AbstractTransactionalTestNGSpringContextTests;
+import org.testng.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.testng.AbstractTransactionalTestNGSpringContextTests;
+import com.smart.domain.User;
+import static org.testng.Assert.*;
 
 /**
  * Created by Administrator on 2017/7/25/025.
  */
 @ContextConfiguration("classpath*:/smart-context.xml")// 启动spring容器
-class UserServiceTest extends AbstractTransactionalTestNGSpringContextTests {
-
+public class UserServiceTest {
+//    @Autowired
     private UserService userService;
 
+    @Autowired
     public void setUserService(UserService userService) {
         this.userService = userService;
     }
+
     @Test
     public void hashMatchUser() {
-        boolean bi = userService.hasMatchUser("admin","123456");
+        boolean b1 = userService.hasMatchUser("admin","123456");
         boolean b2 = userService.hasMatchUser("admin","1111");
 
-        Assert.assertTrue();
-        Assert.assertTrue();
-
-
-
+//        Assert.assertTrue(b1);
+        assertTrue(b1);
+//        Assert.assertTrue(!b2);
     }
-
-
-
-
+    @Test
+    public void findUserByUserName() {
+        User user = userService.findUserByUserName("admin");
+    }
 }
